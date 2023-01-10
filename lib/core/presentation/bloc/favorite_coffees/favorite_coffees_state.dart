90788a223ffe909038ab16c57a88a96acaca6738
crefter <crefter1@yandex.ru>
2023-01-10 23:03:17 +0700

    Add error state for favorite coffees bloc

--- lib/core/presentation/bloc/favorite_coffees/favorite_coffees_state.dart
+++ lib/core/presentation/bloc/favorite_coffees/favorite_coffees_state.dart
@@ -4,9 +4,13 @@ part of 'favorite_coffees_bloc.dart';
 class FavoriteCoffeesState with _$FavoriteCoffeesState {
   const factory FavoriteCoffeesState.empty() = _FavoriteCoffeesStateEmpty;
 
-  const factory FavoriteCoffeesState.loading() =
-      _FavoriteCoffeesStateLoading;
+  const factory FavoriteCoffeesState.loading() = _FavoriteCoffeesStateLoading;
 
   const factory FavoriteCoffeesState.loaded(List<Coffee> favoriteCoffees) =
       _FavoriteCoffeesStateLoaded;
+
+  const factory FavoriteCoffeesState.error(
+    List<Coffee> favoriteCoffees,
+    String message,
+  ) = _FavoriteCoffeesStateError;
 }
